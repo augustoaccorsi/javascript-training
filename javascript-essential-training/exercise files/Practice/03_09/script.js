04eb098cb111e5499334ecb03d700da6e2167fd1
I851169 <augusto.accorsi@sap.com>
2022-02-05 13:38:56 -0300

    add code for exercise 3.9

--- javascript-essential-training/exercise files/Practice/03_09/script.js
+++ javascript-essential-training/exercise files/Practice/03_09/script.js
@@ -16,6 +16,7 @@ const backpack = {
     left: 26,
     right: 26,
   },
+  lidOpen: false,
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
@@ -23,4 +24,17 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  setName: function (name) {
+    this.name = name;
+  },
+  setVolume: function (volume) {
+    this.volume = volume;
+  },
+  setColor: function (color) {
+    this.color = color;
+  },
+  setPockets: function (pocketNum) {
+    this.pocketNum =pocketNum;
+  }
 };
+
