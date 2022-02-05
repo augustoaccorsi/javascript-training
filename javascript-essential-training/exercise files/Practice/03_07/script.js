174a3ec94710e30455d8335d2d116e7db903f193
I851169 <augusto.accorsi@sap.com>
2022-02-05 13:23:31 -0300

    add practice exercise

--- javascript-essential-training/exercise files/Practice/03_07/script.js
+++ javascript-essential-training/exercise files/Practice/03_07/script.js
@@ -7,3 +7,21 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const computer = {
+    type: "laptop",
+    model: "Mac Book Pro",
+    screen: {
+        size: 16,
+        display: "lcd"
+    },
+    memory: 256,
+    keyboardLight: true,
+    setKeyboardLight: function (light){
+        this.keyboardLight = light;
+    }
+};
+
+console.log("Computer object:", computer);
+console.log(computer.type);
